8fff74eaf035f8d930b728aaa9ed53b79682b793
Jude Harding <jharding@genesisgroupusa.com>
2017-05-08 22:51:37 -0400

    modified js file caps error

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ var arr = [1, 2, 3];
 
 arr.length; // 3
 
-Delete arr[1]; // removes "2" since it is a zero based array
+delete arr[1]; // removes "2" since it is a zero based array
 
-Arr[1]; // is undefined
+arr[1]; // is undefined
 
-Arr.length; // is still 3 because there is a HOLE in the array (sparse array)
-// to delete the hole, you need to use SPLICE and the array will adjust
-// you can't delete a variable defined with VAR (exept in console)
+arr.length; // is still 3 WHY
