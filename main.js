6f0b5ba99dd5c6c45f6187901301bc50b5fa0a08
Jude Harding <jharding@genesisgroupusa.com>
2017-05-09 11:20:44 -0400

    notes from todays meeting

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ arr[1];
 
 arr.length;
 // is still 3 WHY
+
+var remove = arr.splice(1, 1);
+
+arr.lenght;
+
+// notes from today's meeting
+//
+// console.log('Hello, world!');
+//
+// let arr = [];
+//
+// const populateArray = () => {
+//     arr = [1, 2, 3, 4, 5];
+// };
+//
+// populateArray();
+//
+// console.log('our untouched array: ', arr);
+//
+// delete arr[1]
+//
+// console.log('deleted value: ', arr[1]);
+//
+// console.log('our new array: ', arr);
+//
+// populateArray();
+//
+// console.log('our untouched array length: ', arr.length);
+//
+// arr.splice(1,1);
+//
+// console.log('our spliced arr: ', arr);
+// console.log('arr length after splice', arr.length);
