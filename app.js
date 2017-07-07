15dfdf1f797a859e4e00f3163afe826f4aed3913
Jude Harding <jharding@genesisgroupusa.com>
2017-07-06 21:10:59 -0400

    days old

---
+++ app.js
@@ -0,0 +1,32 @@
+// block for first name
+
+
+// document.getElementById("fname").addEventListener("blur", myFunction1);
+//
+// function sayHello() {
+//     // var firstname = document.myForm.fname.value;
+//     var firstname = document.getElementById("fname").value;
+//     document.getElementById("firstname").innerHTML;
+//
+// }
+//
+// document.getElementById("email").addEventListener("blur", myFunction2);
+//
+// function myFunction2() {
+//     // var firstname = document.myForm.fname.value;
+//     var email = document.getElementById("email").value;
+//     console.log(email);
+// }
+//
+// document.getElementById("birthday").addEventListener("blur", myFunction3);
+//
+// function myFunction3() {
+//     // var firstname = document.myForm.fname.value;
+//     var birthday = document.getElementById("birthday").value;
+//     console.log(birthday);
+// }
+
+//
+// <html>
+// <p>Welcome </p> + document.write(firstname);
+// </html>
